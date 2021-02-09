ad55f39b7b5ae9fd31d8f778aff414457b63946b
Park Changyong <tempus157@gmail.com>
2021-02-10 01:58:52 +0900

    도움말 추가

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 
 namespace GearRatioCalculator {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+    internal static class Program {
+        private const string Name = "GearRatioCalculator.exe";
+
+        private static void Main(string[] args) {
+            if (args.Length < 3) {
+                WriteHelp();
+                return;
+            }
+
+            Console.WriteLine("Hello, world!");
+        }
+
+        private static void WriteHelp() {
+            Console.WriteLine($"Usage: {Name} {{Count of gears}} {{First gear's ratio}} {{Last gear's ratio}}");
+            Console.WriteLine($"   Ex: {Name} 6 2.89 0.78");
         }
     }
 }
