2500f2d9e24d0a8561a394f846ee796f31ad80ab
Park Changyong <tempus157@gmail.com>
2021-02-10 01:57:21 +0900

    프로젝트 생성

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GearRatioCalculator {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
